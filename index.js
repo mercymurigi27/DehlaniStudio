ca08e1bd9552bf547e51300f78e954074017b19d
mercymurigi27 <mercy.murigi@student.moringaschool.com>
2022-03-16 02:27:57 +0300

    add toggle effects

---
+++ index.js
@@ -0,0 +1,15 @@
+$(".togl1").hide();
+$(".togl, .togl1").click(function(){
+    $(".togl, .togl1").toggle();
+});
+
+$(".togl3").hide();
+$(".togl2, .togl3").click(function(){
+    $(".togl2, .togl3").toggle();
+});
+
+$(".togl5").hide();
+$(".togl4, .togl5").click(function(){
+    $(".togl4, .togl5").toggle();
+});
+
