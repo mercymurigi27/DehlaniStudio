a92558da60ea332eb5b9c35488641dfc1146f0c3
mercymurigi27 <mercy.murigi@student.moringaschool.com>
2022-03-16 05:01:32 +0300

    everything

--- index.js
+++ index.js
@@ -13,3 +13,50 @@ $(".togl4, .togl5").click(function(){
     $(".togl4, .togl5").toggle();
 });
 
+$(".see").hide();
+$(".hob1, .see").hover(function(){
+    $(".hob1, .see").toggle();
+})
+
+$(".see1").hide();
+$(".hob2, .see1").hover(function(){
+    $(".hob2, .see1").toggle();
+})
+
+$(".see2").hide();
+$(".hob3, .see2").hover(function(){
+    $(".hob3, .see2").toggle();
+})
+
+
+$(".see3").hide();
+$(".hob4, .see3").hover(function(){
+    $(".hob4, .see3").toggle();
+})
+
+
+$(".see4").hide();
+$(".hob5, .see4").hover(function(){
+    $(".hob5, .see4").toggle();
+})
+
+$(".see5").hide();
+$(".hob6, .see5").hover(function(){
+    $(".hob6, .see5").toggle();
+})
+
+$(".see6").hide();
+$(".hob7, .see6").hover(function(){
+    $(".hob7, .see6").toggle();
+})
+
+$(".see7").hide();
+$(".hob8, .see7").hover(function(){
+    $(".hob8, .see7").toggle();
+})
+
+
+$('form').submit(()=>{
+    let userName= $('#user-name').val();
+    alert(`Success! Dear ${userName}, Your response has been received. Thank you for contacting Delani Studio.`)
+  })
